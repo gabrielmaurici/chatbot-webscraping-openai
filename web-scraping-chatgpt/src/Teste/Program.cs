92b6b80ea49524c42e699a58895ebfc9771ebcbc
gabriel <gabriel.maurici@hotmail.com>
2024-04-04 22:45:54 -0300

    deletado projeto teste

--- web-scraping-chatgpt/src/Teste/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using WebScraping.ChatGpt.Infrastructure;
-using WebScraping.ChatGpt.Infrastructure.Services;
-
-var teste = new WebScrapingNextMatchService();
-
-var response = await teste.ExecuteScraping();
-Console.WriteLine(response.ToString());
-
