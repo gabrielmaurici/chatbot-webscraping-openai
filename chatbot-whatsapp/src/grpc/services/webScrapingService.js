7c9b45f118a65ce36f50e8cd4041a5aa7f019041
gabriel <gabriel.maurici@hotmail.com>
2024-04-15 22:29:26 -0300

    add camada application e alterado serviços para usar memory cache ao buscar a última e próxima partida

--- chatbot-whatsapp/src/grpc/services/webScrapingService.js
+++ chatbot-whatsapp/src/grpc/services/webScrapingService.js
@@ -5,13 +5,13 @@ const protoLoader = require('@grpc/proto-loader');
 async function getClientGrpc() {
   try {
     return new Promise((resolve, reject) => {
-        const protoFilePath = path.join(__dirname, '../protos/webScraping.proto');
-        const packageDefinition = protoLoader.loadSync(protoFilePath);
-        const webScrapingProto = grpc.loadPackageDefinition(packageDefinition).webScraping;
-        const client = new webScrapingProto.WebScraping('localhost:5100', grpc.credentials.createInsecure());
-        resolve(client);
+      const protoFilePath = path.join(__dirname, '../protos/webScraping.proto');
+      const packageDefinition = protoLoader.loadSync(protoFilePath);
+      const webScrapingProto = grpc.loadPackageDefinition(packageDefinition).webScraping;
+      const client = new webScrapingProto.WebScraping('localhost:5100', grpc.credentials.createInsecure());
+      resolve(client);
     });
-} catch (error) {
+  } catch (error) {
     throw error;
   }
 }
