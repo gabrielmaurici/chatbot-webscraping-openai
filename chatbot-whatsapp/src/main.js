a937094dc6557b98f30910e59ce00450dcf38fd8
gabriel <gabriel.maurici@hotmail.com>
2024-05-24 12:52:16 -0300

    alterado configurações chatbot

--- chatbot-whatsapp/src/main.js
+++ chatbot-whatsapp/src/main.js
@@ -1,4 +1,5 @@
-const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
+require('dotenv').config();
+const { Client, RemoteAuth, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const botMenuService = require('./services/botMenuService');
 const lastMatchService = require('./services/lastMatchService');
@@ -6,60 +7,85 @@ const nextMatchService = require('./services/nextMatchService');
 const chatGptService = require('./services/chatGptService');
 const aiImageGenerateService = require('./services/aiImageGenerateService');
 
-const client = new Client({
-    authStrategy: new LocalAuth(),
-    webVersionCache: {
-        type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2409.0.html',
-    },
-    puppeteer: {
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    }
-});
+const { MongoStore } = require('wwebjs-mongo');
+const mongoose = require('mongoose');
 
-client.once('ready', () => {
-    console.log('Client is ready!');
-});
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    const store = new MongoStore({ mongoose: mongoose });
+    const client = new Client({
+        authStrategy: new RemoteAuth({
+            store: store,
+            backupSyncIntervalMs: 300000
+        }),
+        webVersionCache: {
+            type: 'remote',
+            remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2409.0.html',
+        },
+        puppeteer: {
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        }
+    });
 
-client.on('qr', (qr) => {
-    qrcode.generate(qr, {small: true});
-});
-
-client.on('message_create', async (message) => {
-    const botMenuMessage = botMenuService.checkIfMessageRequestsBotMenu(message.body);
-    if(botMenuMessage){
-        message.reply(botMenuMessage);
-    }
+    client.once('ready', () => {
+        console.log('Client is ready!');
+    });
     
-    const lastMatchMessage = await lastMatchService.checkIfMessageRequestsLastMatch(message.body);
-    if(lastMatchMessage){
-        message.reply(lastMatchMessage);
-    }
-
-    const nextMatchMessage = await nextMatchService.checkIfMessageRequestsNextMatch(message.body);
-    if(nextMatchMessage){
-        message.reply(nextMatchMessage)
-    }
-
-    const askQuestionIAMessage = await chatGptService.checkIfMessageRequestsAskQuestionsIA(message.body);
-    if(askQuestionIAMessage){
-        message.reply(askQuestionIAMessage)
-    }
-
-    const aiImageGenerateMessage = await aiImageGenerateService.generacheckIfMessageRequestsAIImageGenerate(message.body);
-    if(aiImageGenerateMessage){
-        if(!aiImageGenerateMessage.succes){
-            message.reply(aiImageGenerateMessage.message)
-            return;
+    client.on('qr', (qr) => {
+        qrcode.generate(qr, {small: true});
+    });
+    
+    client.on('message_create', async (message) => {
+        const botMenuMessage = botMenuService.checkIfMessageRequestsBotMenu(message.body);
+        if(botMenuMessage){
+            message.reply(botMenuMessage);
         }
-
-        const optionsMedia = {
-            unsafeMime: true
+        
+        const lastMatchMessage = await lastMatchService.checkIfMessageRequestsLastMatch(message.body);
+        if(lastMatchMessage){
+            message.reply(lastMatchMessage);
         }
-        const media = await MessageMedia.fromUrl(aiImageGenerateMessage.url, optionsMedia);
-        await message.reply(media);
-    }
+    
+        const nextMatchMessage = await nextMatchService.checkIfMessageRequestsNextMatch(message.body);
+        if(nextMatchMessage){
+            message.reply(nextMatchMessage)
+        }
+    
+        const askQuestionIAMessage = await chatGptService.checkIfMessageRequestsAskQuestionsIA(message.body);
+        if(askQuestionIAMessage){
+            message.reply(askQuestionIAMessage)
+        }
+    
+        const aiImageGenerateMessage = await aiImageGenerateService.generacheckIfMessageRequestsAIImageGenerate(message.body);
+        if(aiImageGenerateMessage){
+            if(!aiImageGenerateMessage.succes){
+                message.reply(aiImageGenerateMessage.message)
+                return;
+            }
+    
+            const optionsMedia = {
+                unsafeMime: true
+            }
+            const media = await MessageMedia.fromUrl(aiImageGenerateMessage.url, optionsMedia);
+            await message.reply(media);
+        }
+    });
+    
+    client.initialize();
 });
 
-client.initialize();
+
+
+
+// const client = new Client({
+//     authStrategy: new LocalAuth(),
+//     webVersionCache: {
+//         type: 'remote',
+//         remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2409.0.html',
+//     },
+//     puppeteer: {
+//         headless: true,
+//         args: ['--no-sandbox', '--disable-setuid-sandbox']
+//     }
+// });
+
